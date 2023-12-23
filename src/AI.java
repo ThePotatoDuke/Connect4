30b207033e801fd61564575b6d7acdba57d75b80
Mustafa <gameacedamy@gmail.com>
2023-12-23 13:13:07 +0300

    Added evaluate window and center column is more important for heuristic now

--- src/AI.java
+++ src/AI.java
@@ -7,7 +7,7 @@ public class AI {
     public int pickBestMove(int piece){
         Random random = new Random();
         int bestCol = random.nextInt(Connect4.gameBoard[0].length);
-        int bestScore = 0;
+        int bestScore = Integer.MIN_VALUE;
         for (int colNum:validCols(Connect4.gameBoard)) {
             int[][] boardCopy = cloneArray(Connect4.gameBoard);
             dropDiscToCopy(colNum,piece,boardCopy);
@@ -21,49 +21,50 @@ public class AI {
     }
   
     private int scorePosition(int piece, int[][] boardCopy){
+        int rowCount = boardCopy.length;
+        int columnCount = boardCopy[0].length;
+        int windowLength = 4;
+
         int score = 0;
-       
+
+        //Center is better. Look at center column
+        int[] centerCol = new int[boardCopy.length];
+        for (int i = 0; i < boardCopy.length; i++) {
+            centerCol[i] = boardCopy[i][boardCopy[0].length/2];
+        }
+        score += 6 * countOccurrences(centerCol,piece);
+
         //horizontal
-        for (int rowctr = 0; rowctr < boardCopy.length; rowctr++){
+        for (int rowctr = 0; rowctr < rowCount; rowctr++){
             int[] rowArray = boardCopy[rowctr];
             if (Arrays.stream(rowArray).allMatch(value -> value==0)){
                 continue;
             }
-            for (int colCtr= 0; colCtr < boardCopy[0].length-3; colCtr++){
+            for (int colCtr= 0; colCtr < columnCount-3; colCtr++){
                 int[] window = Arrays.copyOfRange(rowArray,colCtr,colCtr+4);
-                if (countOccurrences(window,piece) ==4){
-                    score += 100;
-                } else if (countOccurrences(window,piece)==3 && countOccurrences(window,0) == 1 ) {
-                    score += 10;
-                }
+                score += evaluateWindow(window,piece);
 
             }
         }
 
         //vertical
-        for (int colctr = 0; colctr < boardCopy[0].length; colctr++){
-            int[] colArray = new int[boardCopy.length];
-            for (int i = 0; i < boardCopy.length; i++) {
+        for (int colctr = 0; colctr < columnCount; colctr++){
+            int[] colArray = new int[rowCount];
+            for (int i = 0; i < rowCount; i++) {
                 colArray[i] = boardCopy[i][colctr];
             }
             if (Arrays.stream(colArray).allMatch(value -> value==0)){
                 continue;
             }
-            for (int colCtr= 0; colCtr < boardCopy.length-3; colCtr++){
+            for (int colCtr= 0; colCtr < rowCount-3; colCtr++){
                 int[] window = Arrays.copyOfRange(colArray,colCtr,colCtr+4);
-                if (countOccurrences(window,piece) ==4){
-                    score += 100;
-                } else if (countOccurrences(window,piece)==3 && countOccurrences(window,0) == 1 ) {
-                    score += 10;
-                }
+                score += evaluateWindow(window,piece);
 
             }
         }
 
-        int windowLength = 4;
-        int rowCount = boardCopy.length;
-        int columnCount = boardCopy.length;
-        //positive slope diagnols
+
+        //positive slope diagonals
         for (int r = 0; r < rowCount - windowLength + 1; r++) {
             for (int c = 0; c < columnCount - windowLength + 1; c++) {
                 int[] window = new int[windowLength];
@@ -72,13 +73,7 @@ public class AI {
                     window[i] = boardCopy[r + i][c + i];
                 }
 
-                if (countOccurrences(window, piece) == 4) {
-                    score += 100;
-                }
-
-                else if (countOccurrences(window, piece) == 3 && countOccurrences(window, 0) == 1) {
-                    score += 10;
-                }
+                score += evaluateWindow(window,piece);
             }
         }
 
@@ -94,17 +89,29 @@ public class AI {
                     window[i] = boardCopy[r + 3 -i][c + i];
                 }
 
-                if (countOccurrences(window, piece) == 4) {
-                    score += 100;
-                }
-
-                else if (countOccurrences(window, piece) == 3 && countOccurrences(window, 0) == 1) {
-                    score += 10;
-                }
+                score += evaluateWindow(window,piece);
             }
         }
         
         
+        return score;
+    }
+    private int evaluateWindow(int[] window, int piece){
+        int score = 0;
+        int opponentPiece = - piece;
+
+        if (countOccurrences(window, piece) == 4) {
+            score += 100;
+        }
+        else if (countOccurrences(window, piece) == 3 && countOccurrences(window, 0) == 1) {
+            score += 20;
+        }
+        else if (countOccurrences(window, piece) == 2 && countOccurrences(window, 0) == 2) {
+            score += 5;
+        }
+        else if (countOccurrences(window, opponentPiece) == 3 && countOccurrences(window, 0) == 1) {
+            score -= 100;
+        }
         return score;
     }
     private static int countOccurrences(int[] array, int number) {
